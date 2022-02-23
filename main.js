7a175c32221fa33135512838781b3339277e924c
Lucas Keiti <lucaskeiti2010@gmail.com>
2022-02-23 17:42:10 -0300

    Update testimonials and add contact class

--- main.js
+++ main.js
@@ -29,3 +29,35 @@ window.addEventListener('scroll', function () {
         header.classList.remove('scroll')
     }
 })
+
+const swiper = new Swiper ('.swiper-container',) {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+

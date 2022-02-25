1a03d31d56141c9981509f90a4a516f7eba1a70d
Lucas Keiti <lucaskeiti2010@gmail.com>
2022-02-24 23:11:10 -0300

    Finished Testimonials and some process

--- main.js
+++ main.js
@@ -1,63 +1,60 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-    element.addEventListener('click', function() {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
 
-/*Quando clicar em um item do menu, esconder o menu */
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-    link.addEventListener('click', function () {
-        nav.classList.remove('show')
-    })
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
 
-/* Mudar o header da página quando der scroll */
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
 window.addEventListener('scroll', function () {
-    if(window.scrollY >= navHeight) {
-        // scroll é marioe que a altura do header
-        header.classList.add('scroll')
-    } else {
-        // menor que a altura do header
-        header.classList.remove('scroll')
-    }
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
 })
 
-const swiper = new Swiper ('.swiper-container',) {
-    slidesPerView: 1,
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    mousewheel: true,
-    keyboard: true
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
 })
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
 
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)

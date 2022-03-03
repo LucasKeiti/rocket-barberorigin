91704f740d932210d51d6a5f1d637b744e1a605d
Lucas Keiti <lucaskeiti2010@gmail.com>
2022-03-02 23:07:47 -0300

    Initiating footer styles and func backtotop

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -29,7 +29,7 @@ window.addEventListener('scroll', function () {
     // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -54,7 +54,27 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+// Botão para voltar para o topo 
+
+
+
+function backToTop() {
+  const backToTopButton = document.querySelector ('.back-to-top')
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+}
+
+/* Agrupando uma função para rodar demais funções criadas */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
